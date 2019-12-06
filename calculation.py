e2724e4416ebcfdee0853adb703434cd3044d584
martinsedighi <55633266+martinsedighi@users.noreply.github.com>
2019-12-05 22:46:01 -0800

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import csv
+import os
+ #load and Output
+    file_to_load = os.path.join("budget_data.csv")
+    file_to_output = os.path.join("budget_annaalysis.txt")     
+
